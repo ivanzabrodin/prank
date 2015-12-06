aa931b5d613d2031ce4e0275f1a5962d339dd87c
Иван Забродин <zabrodinivan77@gmail.com>
2015-12-06 04:42:30 +0300

    add player

--- js/script.js
+++ js/script.js
@@ -15,5 +15,55 @@ $(document).ready(function() {
     austDay = new Date(austDay.getFullYear(2015), 11, 7);
     $('#countdown').countdown({until: austDay, padZeroes: true, format: "HMS"});
   });
+
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            mp3: ""
+          });
+        },
+        swfPath: "/js",
+        supplied: "mp3,oga",
+        play: function() { // Останавливаем остальные
+            $(this).jPlayer("pauseOthers");
+        },
+        wmode: "window",
+        smoothPlayBar: true,
+        keyEnabled: true
+    });
  
+    $("#jquery_jplayer_2").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            mp3: "" 
+          });
+        },
+        swfPath: "/js",
+        supplied: "mp3,oga",
+        play: function() { // Останавливаем остальные
+            $(this).jPlayer("pauseOthers");
+        },
+        cssSelectorAncestor: "#jp_container_2",
+        wmode: "window",
+        smoothPlayBar: true,
+        keyEnabled: true
+    });
+
+    $("#jquery_jplayer_3").jPlayer({
+        ready: function () {
+          $(this).jPlayer("setMedia", {
+            mp3: "" 
+          });
+        },
+        swfPath: "/js",
+        supplied: "mp3,oga",
+        play: function() { // Останавливаем остальные
+            $(this).jPlayer("pauseOthers");
+        },
+        cssSelectorAncestor: "#jp_container_3",
+        wmode: "window",
+        smoothPlayBar: true,
+        keyEnabled: true
+    });
 });
+ 
